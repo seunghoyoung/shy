cb3e76ef57b4b0ac8c0a8562cae0487fa9ada3c7
seunghoyoung <46391651+seunghoyoung@users.noreply.github.com>
2019-01-05 14:48:36 +0900

    Add files via upload

--- shy/day5/src/window2/Pizza.java
+++ shy/day5/src/window2/Pizza.java
@@ -8,10 +8,12 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.omg.PortableServer.POAPackage.AdapterAlreadyExists;
+import java.awt.Font;
 
 public class Pizza {
 
@@ -25,11 +27,18 @@ public class Pizza {
 	int bul = 0;
 	
 	JTextArea list; 
+	private JLabel lblNewLabel;
+	private final JButton btnNewButton = new JButton("\uB098\uB97C\uB20C\uB7EC\uC918");
 	
 	public Pizza() {
 		f = new JFrame();
-		f.setSize(350, 200);
+		f.setBackground(Color.RED);
+		f.setFont(new Font("휴먼매직체", Font.PLAIN, 12));
+		f.setForeground(Color.RED);
+		f.setSize(338, 227);
 		top = new JLabel("<<<<<<<<<자바 피자에 오신것을 환영합니다. 반갑습니다>>>>>>>");
+		top.setFont(new Font("휴먼매직체", Font.PLAIN, 12));
+		top.setForeground(Color.RED);
 		count = new JLabel("개수");
 		b1 = new JButton("콤보피자");
 	    b2 = new JButton("포테이토피자");
@@ -38,23 +47,34 @@ public class Pizza {
 	    
 	    
 	    FlowLayout flow = new FlowLayout();
-	    f.setLayout(flow);
+	    f.getContentPane().setLayout(flow);
         list = new JTextArea(3, 30);
 	    
 	    
 	    
-	    f.add(top);
-	    f.add(b1);
-	    f.add(b2);
-	    f.add(b3);
-	    f.add(count);
-	    f.add(total);
-	    f.add(list);
+	    f.getContentPane().add(top);
+	    f.getContentPane().add(b1);
+	    f.getContentPane().add(b2);
+	    f.getContentPane().add(b3);
+	    f.getContentPane().add(count);
+	    f.getContentPane().add(total);
+	    f.getContentPane().add(list);
 	    
 	    total.setBackground(Color.green);
 	    b1.setForeground(Color.orange);
 	    b2.setForeground(Color.orange);
 	    b3.setForeground(Color.orange);
+	    
+	    lblNewLabel = new JLabel("\uCD1D\uACC4");
+	    lblNewLabel.setForeground(Color.RED);
+	    f.getContentPane().add(lblNewLabel);
+	    btnNewButton.setBackground(Color.RED);
+	    btnNewButton.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent arg0) {
+	    		JOptionPane.showMessageDialog(null,"나를 정말 누르셨군요");
+	    	}
+	    });
+	    f.getContentPane().add(btnNewButton);
 	    
 	    
 	    
